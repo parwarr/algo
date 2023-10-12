bbba19f2c0b683518bd4d2ef23c71b735c5b3867
parwarr <parwar.habasch@gmx.de>
2023-10-12 05:55:24 +0200

    feat: update

--- test/te.js
+++ test/te.js
@@ -2,11 +2,7 @@ function sum(n) {
     let sum = 0;
     for (let i = 0; i < n; i++) {
         sum += i;
-        console.log(sum +  " " + i);
     }
-
-    console.log("res");
-    console.log(sum);
 }
 
 sum(10);
